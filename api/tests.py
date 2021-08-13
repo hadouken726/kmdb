df0ab1faee23cf092c96d6b049ebf35208daedda
gustavodm <gusttavomota@gmail.com>
2021-08-13 08:45:57 -0300

    test(account view): add post user

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 from django.contrib.auth.models import User
 from api.models import Movie, Genre, Review
 
@@ -74,8 +76,45 @@ class ReviewModelTest(TestCase):
 
     def test_create_movie_success(self):
         self.assertIsNotNone(self.review_instance.id)
-    
 
+
+class AccountViewTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.client = APIClient()
+        cls.route = '/api/accounts'
+        cls.user_data = {
+            "username": "user",
+            "password": "1234",
+            "first_name": "John",
+            "last_name": "Wick",
+            "is_superuser": False,
+            "is_staff": False
+        }
+        
+    
+    def test_user_successfullly_created(self):
+        response = self.client.post(self.route, self.user_data, format='json')
+        user = User.objects.get(username=self.user_data['username'])
+        self.assertIsInstance(user.id, int)
+        expected_response_data = self.user_data.copy()
+        expected_response_data.pop('password')
+        expected_response_data['id'] = user.id
+        self.assertEqual(response.data, expected_response_data)
+
+
+    def test_user_already_exists(self):
+        getted_user = User.objects.filter(username=self.user_data['username']).first()
+        self.assertIsNotNone(getted_user)
+        response = self.client.post(self.route, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'username': ["A user with that username already exists."]})
+        
+
+
+
+        
 
 
 
