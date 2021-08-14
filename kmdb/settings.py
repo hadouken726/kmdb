288f75c6bb406ff93657903096266b73b6faf1b8
gustavodm <gusttavomota@gmail.com>
2021-08-13 23:28:51 -0300

    test: add login view test

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
